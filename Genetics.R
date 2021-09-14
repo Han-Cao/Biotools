5728f369239fbbad94de3478f53f1e8b9654bc25
HanC <caohan7659@gmail.com>
2021-09-14 19:33:02 +0800

    plink2metasoft

--- Genetics.R
+++ Genetics.R
@@ -44,3 +44,36 @@ rankNorm_withNA <- function(u, k = 3/8){
   df_merge <- left_join(df_raw, df_no_NA, by="ID")
   return(df_merge$rankNorm)
 }
+
+
+# convert plink results to METASOFT input
+plink2metasoft <- function(plink_files, corhots){
+	master_table <- data.frame()
+	# read and combine plink results
+	for (i in 1:length(plink_files)) {
+		file = plink_files[i]
+		cohort = cohorts[i]
+		cohort_table <- read.table(file, header=TRUE)
+		cohort_table$cohort <- cohort
+		master_table <- rbind(master_table, cohort_table)
+	}
+	master_table <- filter(master_table, !is.na(BETA), !is.na(SE))
+
+	# variant infomation
+	variant_info <- master_table[,c("SNP", "CHR", "BP", "A1")]
+	variant_info <- filter(variant_info, !duplicated(variant_info)) %>% arrange(BP)
+
+	# merge beta
+	master_table_beta <- master_table[,c("SNP", "cohort", "BETA")]
+	master_table_beta <- dcast(master_table_beta, SNP ~ cohort, value.var="BETA")
+	# merge se
+	master_table_se <- master_table[,c("SNP", "cohort", "SE")]
+	master_table_se <- dcast(master_table_se, SNP ~ cohort, value.var="SE")
+
+	output_table <- data.frame(SNP=master_table_beta$SNP)
+	for (cohort in cohorts) {
+		output_table[,paste0(cohort, "_BETA")] <- master_table_beta[, cohort]
+		output_table[,paste0(cohort, "_SE")] <- master_table_se[, cohort]
+	}
+	return(list(variants<-variant_info, metasoft<-output_table))
+}
